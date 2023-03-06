db4f5780c428d13669130357a42240628597ddfc
Kim Conger <39608848+Kyssuber@users.noreply.github.com>
2023-03-06 12:02:56 -0600

    Update gather_output_params_vf.py

--- my_mucho_galfit/gather_output_params_vf.py
+++ my_mucho_galfit/gather_output_params_vf.py
@@ -156,7 +156,7 @@ if __name__ == '__main__':
         #add row of zeros for ith central galaxy (and each "off-centered" sersic object, if applicable)
         g = output_galaxy(galname=cat['prefix'][i],objname=cat['objname'][i], vfid=cat['VFID'][i], vfid_v1=cat['VFID_V1'][i], convflag=convflag, band=band) 
         num_rows = int(g.ncomp)
-        print(str(conflag))
+        print(str(convflag))
         for num in range(num_rows):
             zero_row = np.zeros(len(dtype))
             zero_row = np.ndarray.tolist(zero_row) #convert to list, as the np array is all floats and won't allow the zeroth index element to be replaced with a string.
