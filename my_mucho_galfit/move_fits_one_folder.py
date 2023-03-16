9c2f04ce39dd8de28701723f120c98f2f515eb46
Kim Conger <39608848+Kyssuber@users.noreply.github.com>
2023-03-16 13:30:32 -0500

    Update move_fits_one_folder.py

--- my_mucho_galfit/move_fits_one_folder.py
+++ my_mucho_galfit/move_fits_one_folder.py
@@ -9,20 +9,31 @@ import numpy as np
 from astropy.table import Table
 
 #should be two per galaxy - rband and w3
-def grab_input_cutouts(catalog, host_folder_path, target_folder):
+def grab_input_cutouts(catalog, input_cutouts_path, target_folder):
   VFIDs = catalog['VFID']
   objnames = catalog['objname']
+  RAs = catalog['RA_1']
   
+  #have to retrieve these images from the RA directories.
   for i in range(len(catalog)):
-    print('Moving '+VFIDs[i])
-    galaxy_folder = host_folder_path+VFIDs[i]+'/'
-    input_cutouts = glob.glob(galaxy_folder+objnames[i]+'-custom-image-*')
+    print(VFIDs[i])
+    ra_int = int(np.floor(RAs[i]))
+    
+    ra_folder = input_cutouts_path+str(ra_int)+'/'
+    galaxy_folder = ra_folder+objnames[i]
+    galaxy_folder_group = ra_folder+objnames[i]+'_GROUP'
+    
+    if os.path.isdir(galaxy_folder):
+      input_cutouts = glob.glob(galaxy_folder+'-custom-image-*')
+    else:
+      input_cutouts = glob.glob(galaxy_folder_group+'-custom-image-*')
     
     for im in input_cutouts:
       print('Moving '+im)
       os.system('cp '+im+' '+target_folder)
   
 #should be four per galaxy - rband (nopsf, psf) and w3 (nopsf, psf)
+#if galfit 'failed', then out* images will not appear in the folder. 
 def grab_output_cutouts(catalog, host_folder_path, target_folder):
   VFIDs = catalog['VFID']
   objnames = catalog['objname']
@@ -37,15 +48,19 @@ def grab_output_cutouts(catalog, host_folder_path, target_folder):
       os.system('cp '+im+' '+target_folder)
 
 if __name__ == '__main__':
+  
   homedir=os.getenv("HOME")
   vf = Table.read(homedir+'/sgacut_coadd.fits')
-  onefolder_path = '/mnt/astrophysics/kconger_wisesize/all_input_fits/'
+  
   host_folder_path = '/mnt/astrophysics/muchogalfit-output/'
+  input_cutouts_path = '/mnt/virgofilaments-data/'
 
+  onefolder_path = '/mnt/astrophysics/kconger_wisesize/all_input_fits/'
+  
   print('Creating target directory '+onefolder_path)
   os.system('mkdir '+onefolder_path)
   
   'Moving postage stamp cutouts for rband and W3...'
-  grab_input_cutouts(vf, host_folder_path, onefolder_path)
+  grab_input_cutouts(vf, input_cutouts_path, onefolder_path)
   'Moving GALFIT output mosaics for rband and w3...'
   grab_output_cutouts(vf, host_folder_path, onefolder_path)
