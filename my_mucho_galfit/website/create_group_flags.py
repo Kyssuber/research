0f22c3054f3745123315acd54d1af646cf88bee3
kconger@ku.edu <kconger@ku.edu>
2023-04-10 21:02:41 +0000

    create group galaxy columns in main table

--- my_mucho_galfit/website/create_group_flags.py
+++ my_mucho_galfit/website/create_group_flags.py
@@ -22,7 +22,7 @@ mask = np.zeros(len(vf),dtype=bool)
 groupGalaxy = np.zeros(len(vf),dtype=bool)
 primaryGroup = np.zeros(len(vf),dtype=bool)
 ncomp = np.zeros(len(vf),dtype=int)
-group_name = np.zeros(len(vf),dtype=str)
+group_name = np.zeros(len(vf),dtype='object')
 
 columns = [groupGalaxy,primaryGroup,ncomp,group_name]
 column_names = ['group_flag','primaryGroup_flag','ncomp','group_name']
@@ -44,7 +44,7 @@ for n in range(len(vf)):
                     primaryGroup[n] = True
                     mask[n] = False
                     primary_pathname = primary_pathnames[primary_counter]
-                    print(primary_pathname)
+                    primary_pathname=str(primary_pathname)  #otherwise object is a 'row' and cannot be split
                     name = primary_pathname.split('/')[-1] #divide pathname into strings that were separated by '/', then isolate group name
                     group_name[n] = name
                     primary_counter+=1   #set up primary counter for the next primary galaxy!
@@ -53,13 +53,14 @@ for n in range(len(vf)):
                     primaryGroup[vf['VFID']==group_vfids[num]] = False
                     mask[vf['VFID']==group_vfids[num]] = False
                     group_name[vf['VFID']==group_vfids[num]] = name  #will have same group name as primary galaxy
-
+        
         else:
             mask[n] = False   #galaxy is part of subsample. will not be masked out.
             primaryGroup[n] = False   #galaxy will not be a primary galaxy, as evidenced by the lack of a .txt file in the directory
             ncomp[n] = 1 if ncomp[n]==0 else ncomp[n]  #if ncomp[n] is already populated by a nonzero integer, lEaVe It AlOnE.
-            group_name[n] = '' if len(group_name[n])==0 else vf['objname'][n]  #if group galaxy, then entry will either not be empty or will be replaced with group name later on. Similar idea as above --> if already populated, LEAVE IT ALONE.
-
+            #print('before',group_name[n])
+            group_name[n] = vf['objname'][n] if group_name[n]==0 else group_name[n]  #if group galaxy, then entry will either not be empty or will be replaced with group name later on. Similar idea as above --> if already populated, LEAVE IT ALONE.
+            #print('after',group_name[n])
         #if ncomp>1, then galaxy is part of a group. full stop.
         groupGalaxy[n] = True if ncomp[n]>1 else False
 
@@ -71,7 +72,14 @@ for n in range(len(vf)):
         group_name[n] = vf['objname'][n]   #let the group name be the objname of the galaxy
 
 for col in range(len(columns)):
-    c=MaskedColumn(columns[col],name=column_names[col],mask=mask)
+    try:
+        vf.remove_column(column_names[col])
+    except:
+        pass
+    if column_names[col]=='group_name':
+        c=MaskedColumn(columns[col],name=column_names[col],mask=mask,dtype=str)
+    else:
+        c=MaskedColumn(columns[col],name=column_names[col],mask=mask)
     vf.add_column(c)
 
 vf.write(path_to_table+'v2_snrcoadd.fits',overwrite=True)
