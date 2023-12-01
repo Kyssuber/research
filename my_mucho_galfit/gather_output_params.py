4c531716bb45def924a5215f86943918afcfcbbc
kconger@ku.edu <kconger@ku.edu>
2023-12-01 19:25:13 +0000

    failsafes for group galaxies that have failed in one band or another

--- my_mucho_galfit/gather_output_params.py
+++ my_mucho_galfit/gather_output_params.py
@@ -39,8 +39,13 @@ class output_galaxy:
         self.galfit_dir=galfit_dir
         self.vfid_dir=self.galfit_dir+self.vfid+'/'
 
-        #if band is an integer, than prepend with w to indicate that the 'band' is a WISE channel
         self.band = band
+
+        #I guess 1+ sersic obj fitting is not set up for W1 yet, so this is a failsafe.
+        if self.ncomp>1:
+            print(self.ncomp)
+            print(self.ncomp)
+            print(self.ncomp)
 
         outimage_path = self.vfid_dir+str(self.objname)+'*'+str(self.band)+'-out1.fits'
         if convflag==str(1):
@@ -54,8 +59,7 @@ class output_galaxy:
         
     #takes GALFIT .fits output header and extracts the parameter/error values, outputs list of fit parameters
     def parse_galfit(self,printflag=False):
-        
-        #the following assumes that the maximum number of VFID sersic objects in a given cutout is 4.
+        #the following assumes that the maximum number of VFID sersic objects in a given cutout is 5.
         if self.ncomp == 1:
             header_keywords1=['1_XC','1_YC','1_MAG','1_RE','1_N','1_AR','1_PA','2_SKY','CHI2NU']
             header_keywords=[header_keywords1]
@@ -81,6 +85,18 @@ class output_galaxy:
             header_keywords4=['4_XC','4_YC','4_MAG','4_RE','4_N','4_AR','4_PA','6_SKY','CHI2NU']
             header_keywords5=['5_XC','5_YC','5_MAG','5_RE','5_N','5_AR','5_PA','6_SKY','CHI2NU']
             header_keywords=[header_keywords1,header_keywords2,header_keywords3,header_keywords4,header_keywords5]
+        if self.ncomp == 9:
+            header_keywords1=['1_XC','1_YC','1_MAG','1_RE','1_N','1_AR','1_PA','10_SKY','CHI2NU']
+            header_keywords2=['2_XC','2_YC','2_MAG','2_RE','2_N','2_AR','2_PA','10_SKY','CHI2NU']
+            header_keywords3=['3_XC','3_YC','3_MAG','3_RE','3_N','3_AR','3_PA','10_SKY','CHI2NU']
+            header_keywords4=['4_XC','4_YC','4_MAG','4_RE','4_N','4_AR','4_PA','10_SKY','CHI2NU']
+            header_keywords5=['5_XC','5_YC','5_MAG','5_RE','5_N','5_AR','5_PA','10_SKY','CHI2NU']
+            header_keywords6=['6_XC','6_YC','6_MAG','6_RE','6_N','6_AR','6_PA','10_SKY','CHI2NU']
+            header_keywords7=['7_XC','7_YC','7_MAG','7_RE','7_N','7_AR','7_PA','10_SKY','CHI2NU']
+            header_keywords8=['8_XC','8_YC','8_MAG','8_RE','8_N','8_AR','8_PA','10_SKY','CHI2NU']
+            header_keywords9=['9_XC','9_YC','9_MAG','9_RE','9_N','9_AR','9_PA','10_SKY','CHI2NU']
+            header_keywords=[header_keywords1,header_keywords2,header_keywords3,header_keywords4,header_keywords5,header_keywords6,
+                header_keywords7,header_keywords8,header_keywords9]
 
         fit_parameters=[]
         
@@ -101,7 +117,18 @@ class output_galaxy:
                 temp.append(self.vfid)  #if ncomp=1, simply use the central galaxy's VFID
             
             image_header = fits.getheader(self.outimage,2)   #grab header information from the model image
+            
+            if (self.ncomp>1):
+                sky_num = str(self.ncomp+1)
+                sky_head = sky_num+'_SKY'
+                print(sky_head)
+                if (sky_head not in image_header):
+                    print('keywords not in image_header')
+                    header_keywords = [header_keywords1]
+                    break
+            
             for hkey in header_keywords[n]:
+
                 s=str(image_header[hkey])
             
                 if s.find('[')+1 > 0:   #if a parameter is held fixed (meaning the parameter is enclosed with brackets and does not contain an uncertainty)
@@ -110,7 +137,7 @@ class output_galaxy:
                     s=f'{s} +/- {0.}'   #fit and error
                 
                 t=s.split('+/-')
-
+                
                 try:
                     values=(float(t[0]),float(t[1]))   #fit and error
                     temp.append(values[0])
@@ -169,7 +196,10 @@ if __name__ == '__main__':
         if os.path.exists(galfit_dir_one+'/'+'galsFOV.txt'):
             g.fovtab=ascii.read(galfit_dir_one+'/'+'galsFOV.txt')   #define as class variable; will need for g.parse_galfit()
             g.ncomp=len(g.fovtab)   #the length of this textfile (i.e., number of entries)
-        
+        #print(g.ncomp)
+        #if g.band == 'W1':
+        #    g.ncomp=1          #1+ sersic obj fitting not yet set up for W1. this line is a failsafe.
+
         #next, check whether this galaxy is 'primary' or part of a group (if the latter, then there will be no galfit output in the directory). if not, then the the main loop will proceed onward to the next VFID
         #recall that g.outimage is the FULL pathname to the galfit output file(s) for one galaxy
         if os.path.isfile(g.outimage):
